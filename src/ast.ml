030742b98edbaa8629c9136b9b55837be19f397d
Venkata Yamajala <vsy2104@columbia.edu>
2012-12-09 02:06:51 -0500

    Started working on code generation

--- src/ast.ml
+++ src/ast.ml
@@ -37,3 +37,43 @@ type node = {
 }
 
 type program = node list
+
+let rec string_of_expr = function
+  | Literal(l) -> "Num(" ^ string_of_int(l) ^ ")"
+  | Id(s) -> "Name('" ^ s ^ "', Load())"
+  | Binop(e1, o, e2) ->
+      "BinOp(" ^ string_of_expr e1 ^ ", " ^
+      (match o with
+        Add -> "Add()" | Sub -> "Sub()" | Mult -> "Mult()" | Div -> "Div()" | Mod -> "Mod()"
+      | Eq -> "Eq()" | Neq -> "NotEq()"
+      | Lt -> "Lt()" | Leq -> "LtE()" | Gt -> "Gt()" | Geq -> "GtE()") ^ ", " ^ 
+      string_of_expr e2 ^ ")"
+  | Assign(v, e) -> "[Name('" ^ v ^ "), Store())]" ^  string_of_expr e
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | Noexpr -> ""
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | Return(expr) -> "Return(" ^ string_of_expr expr ^ ")"
+  | If(e, s, Block([])) -> "If(" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If(e, s1, s2) ->  "If(" ^ string_of_expr e ^ "," ^ "[" ^
+      string_of_stmt s1 ^ "], " ^ ",[" ^ string_of_stmt s2 ^ "])"
+  | For(e1, e2, e3, s) ->
+      "For(" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "While(" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_vdecl id = "int " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  fdecl.fname ^ "(" ^ String.concat ", " fdecl.formals ^ ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_program (vars, funcs) =
+  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
